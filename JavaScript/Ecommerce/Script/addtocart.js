23b2ec3d0ab9652dddb0f722ce7721654a62fc6b
MindofSkk <helpmeskk@gmail.com>
2022-09-08 09:11:28 +0530

    filterpart done:

--- JavaScript/Ecommerce/Script/addtocart.js
+++ JavaScript/Ecommerce/Script/addtocart.js
@@ -1,20 +1,56 @@
-console.log('hello')
+console.log("hello");
 
-var cartdata=JSON.parse(localStorage.getItem("cartdata"))
-console.log(cartdata)
-cartdata.map(function (elem, inde) {
+var cartdata = JSON.parse(localStorage.getItem("cartdata"));
+console.log(cartdata);
+
+function showdata(cartdata) {
+  cartdata.map(function (elem, index) {
     var div = document.createElement("div");
-  
+
     var img = document.createElement("img");
     img.src = elem.image;
     var txt = document.createElement("p");
     txt.innerText = elem.name;
     var price = document.createElement("p");
-    price.innerText = "Rs  " + elem.price;
+    price.innerText = "Rs  " + elem.price + "  -  " + elem.cartQuantity;
     var btn = document.createElement("button");
-    btn.innerText = "+";
-    
+    btn.innerText = "add +";
+    btn.addEventListener("click",function()
+    {
+      increseqauntity(index)
+    })
+
     div.append(img, txt, price, btn);
-  
+
     document.querySelector("#container").append(div);
   });
+}
+
+showdata(cartdata);
+
+
+function  increseqauntity(index)
+{
+   cartdata[index].cartQuantity++;   
+   console.log(cartdata) ;
+   
+   
+   
+   showdata(cartdata);
+
+
+}
+
+
+
+
+
+var total = cartdata.reduce(function (acc, item) {
+  // console.log(item.price,acc)
+  return acc + item.price;
+}, 0);
+
+
+console.log(total);
+document.getElementById("totalprice").innerText =
+  "Total Amout : Rs " + total + " only";
--- JavaScript/Ecommerce/Script/index.js
+++ JavaScript/Ecommerce/Script/index.js
@@ -4,7 +4,7 @@ var shoes = [
     name: "Shoe 1",
     image:
       "https://images.pexels.com/photos/3281608/pexels-photo-3281608.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 4,
+
     price: 999,
   },
   {
@@ -12,7 +12,7 @@ var shoes = [
     name: "Shoe 2",
     image:
       "https://images.pexels.com/photos/9993286/pexels-photo-9993286.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 799,
   },
   {
@@ -20,7 +20,7 @@ var shoes = [
     name: "Shoe 3",
     image:
       "https://images.pexels.com/photos/8378567/pexels-photo-8378567.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 4,
+
     price: 545,
   },
   {
@@ -28,7 +28,7 @@ var shoes = [
     name: "Shoe 4",
     image:
       "https://images.pexels.com/photos/5710085/pexels-photo-5710085.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 9654,
   },
   {
@@ -36,7 +36,7 @@ var shoes = [
     name: "Shoe 5",
     image:
       "https://images.pexels.com/photos/1456733/pexels-photo-1456733.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 958,
   },
   {
@@ -44,7 +44,7 @@ var shoes = [
     name: "Shoe 6",
     image:
       "https://images.pexels.com/photos/1456706/pexels-photo-1456706.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 455,
   },
   {
@@ -52,7 +52,7 @@ var shoes = [
     name: "Shoe 7",
     image:
       "https://images.pexels.com/photos/2529157/pexels-photo-2529157.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 6556,
   },
   {
@@ -60,7 +60,7 @@ var shoes = [
     name: "Shoe 8",
     image:
       "https://images.pexels.com/photos/4495409/pexels-photo-4495409.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 544,
   },
   {
@@ -68,7 +68,7 @@ var shoes = [
     name: "Shoe 9",
     image:
       "https://images.pexels.com/photos/6540927/pexels-photo-6540927.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 549,
   },
   {
@@ -76,7 +76,7 @@ var shoes = [
     name: "Shoe 10",
     image:
       "https://images.pexels.com/photos/9645566/pexels-photo-9645566.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 799,
   },
   {
@@ -84,7 +84,7 @@ var shoes = [
     name: "Shoe 11",
     image:
       "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 322,
   },
   {
@@ -92,7 +92,7 @@ var shoes = [
     name: "Shoe 12",
     image:
       "https://images.pexels.com/photos/6777980/pexels-photo-6777980.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 654,
   },
   {
@@ -100,7 +100,7 @@ var shoes = [
     name: "Shoe 13",
     image:
       "https://images.pexels.com/photos/6747235/pexels-photo-6747235.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 258,
   },
   {
@@ -108,7 +108,7 @@ var shoes = [
     name: "Shoe 14",
     image:
       "https://images.pexels.com/photos/3261069/pexels-photo-3261069.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 0,
+
     price: 658,
   },
   {
@@ -116,7 +116,7 @@ var shoes = [
     name: "Shoe 15",
     image:
       "https://images.pexels.com/photos/4252948/pexels-photo-4252948.jpeg?auto=compress&cs=tinysrgb&w=800",
-    cart_quantity: 2,
+
     price: 1001,
   },
 ];
@@ -124,34 +124,53 @@ var shoes = [
 localStorage.setItem("shoesdata", JSON.stringify(shoes));
 var data = JSON.parse(localStorage.getItem("shoesdata"));
 
-var cartdata=JSON.parse(localStorage.getItem("cartdata")) || [];
+var cartdata = JSON.parse(localStorage.getItem("cartdata")) || [];
 
+function appenddata(data) {
+  console.log("map", data);
+  data.map(function (elem, inde) {
+    var div = document.createElement("div");
 
-data.map(function (elem, inde) {
-  var div = document.createElement("div");
+    var img = document.createElement("img");
+    img.src = elem.image;
+    var txt = document.createElement("p");
+    txt.innerText = elem.name;
+    var price = document.createElement("p");
+    price.innerText = "Rs  " + elem.price;
+    var btn = document.createElement("button");
+    btn.innerText = "Add To Cart";
+    btn.addEventListener("click", function () {
+      addtocart(elem);
+    });
+    div.append(img, txt, price, btn);
 
-  var img = document.createElement("img");
-  img.src = elem.image;
-  var txt = document.createElement("p");
-  txt.innerText = elem.name;
-  var price = document.createElement("p");
-  price.innerText = "Rs  " + elem.price;
-  var btn = document.createElement("button");
-  btn.innerText = "Add To Cart";
-  btn.addEventListener("click",function(){
-    addtocart(elem)
-  })
-  div.append(img, txt, price, btn);
+    document.querySelector("#container").append(div);
+  });
+}
 
-  document.querySelector("#container").append(div);
-});
+appenddata(data);
+function addtocart(elem) {
+  var p = elem;
+  p.cartQuantity = 1;
+  cartdata.push(p);
 
-
-function addtocart(elem)
-{
-
-    var p=elem;
-    cartdata.push(p)
-  
-  localStorage.setItem("cartdata",JSON.stringify(cartdata)) 
+  localStorage.setItem("cartdata", JSON.stringify(cartdata));
+}
+function Sortitem() {
+  var short = document.getElementById("sorter").value;
+  console.log(short);
+  //  console.log(data)
+  if (short === "HtoL") {
+    var newdata = data.sort(function (a, b) {
+      return b.price - a.price;
+    });
+    document.getElementById("container").innerHTML = null;
+    appenddata(newdata);
+  } else if (short === "LtoH") {
+    var newdata = data.sort(function (a, b) {
+      return a.price - b.price;
+    });
+    document.getElementById("container").innerHTML = null;
+    appenddata(newdata);
+  }
 }
